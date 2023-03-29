40db5690e6f212614afa8441e4c8cab9d7815b84
SzymonRem <126695909+SzymonRem@users.noreply.github.com>
2023-03-29 14:19:25 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+    for(int i=0;i<5;i++){
+      for(int j=0;j<4-i;j++){
+        System.out.print(" ");
+      }
+      for(int j=0;j<i+1;j++){
+         System.out.print("*");
+      }
+      System.out.println();
+      }
+    for(int k=0;k<5;k++){
+      for(int l=0;l<5-k;l++){
+        System.out.print("*");
+      }
+       System.out.println();
+    }
+     
+    }
+  }
